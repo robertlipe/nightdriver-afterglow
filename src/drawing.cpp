d09b4e4ca73c3c35523e699489fd71f7326b78b0
Robert Lipe <robertlipe@gmail.com>
2026-06-27 04:21:20 -0500

    Add defensive check before calling DrawCaptionOverlay

--- src/drawing.cpp
+++ src/drawing.cpp
@@ -318,7 +318,7 @@ void IRAM_ATTR DrawLoopTaskEntry(void *)
             g_ptrSystem->GetEffectManager().ReportNewFrameAvailable();
         }
 
-        if (g_ptrSystem->GetEffectManager().GetCurrentEffect().ShouldShowTitle())
+        if (graphics && g_ptrSystem->GetEffectManager().GetCurrentEffect().ShouldShowTitle())
         {
             graphics->DrawCaptionOverlay();
         }
