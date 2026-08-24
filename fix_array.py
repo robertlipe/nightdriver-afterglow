a8531f1c02c27d7606b32bfd93c823965e8fbc6c
robertlipe <8724975+robertlipe@users.noreply.github.com>
2026-08-24 21:55:12 +0000

    Fix class template argument deduction issue with API route array

--- fix_array.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open("src/webserver.cpp", "r") as f:
-    content = f.read()
-
-content = content.replace("    }};\n\n    const std::array<ApiRoute, 19> routes {{", "    };\n\n    const std::array<ApiRoute, 19> routes {{")
-
-with open("src/webserver.cpp", "w") as f:
-    f.write(content)
