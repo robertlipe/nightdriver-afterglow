36b6dab3c73a5080e9be52c9371be733a27e6a7c
robertlipe <8724975+robertlipe@users.noreply.github.com>
2026-08-24 21:16:52 +0000

    Fix template deduction error by adding ApiRoute tags explicitly

--- fix_array.py
+++ fix_array.py
@@ -3,7 +3,7 @@ import sys
 with open("src/webserver.cpp", "r") as f:
     content = f.read()
 
-content = content.replace("ApiRoute{\"/effects\"", "{\"/effects\"")
+content = content.replace("    }};\n\n    const std::array<ApiRoute, 19> routes {{", "    };\n\n    const std::array<ApiRoute, 19> routes {{")
 
 with open("src/webserver.cpp", "w") as f:
     f.write(content)
